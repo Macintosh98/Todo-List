62f74658389bca6ac7f3402a473590b560a48043
Abhishek Linuxer <abhisheklinuxer@localhost.localdomain>
2021-05-16 13:36:54 +0530

    first commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(express.static('public'))
 
-const port=process.env.PORT | 5000;
+const port=process.env.PORT || 5000;
 app.listen(port,()=>{
     console.log("server started")
 })
